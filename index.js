63e3d00956bc902cd1b718b0a15618884f8e4ffc
cloudtechdevsoul <139046669+cloudtechdevsoul@users.noreply.github.com>
2023-07-23 16:34:16 +0500

    new code

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
 const ytdl = require('ytdl-core');
-const fs = require('fs')
+const fs = require('fs');
 var router = express.Router();
-router.get('/', function (req, res, next) {
-    console.log('rputer calld')
+
+router.get('/', async function (req, res, next) {
+    console.log('router called');
     let url = "https://www.youtube.com/watch?v=S9atRW1DgbQ";
-    ytdl(url).pipe(fs.createWriteStream('video.mp4'));
-    res.end();
-})
-app.use(router);
+
+    const videoReadableStream = ytdl(url);
+    const videoWritableStream = fs.createWriteStream('video.mp4');
+
+    videoReadableStream.pipe(videoWritableStream);
+    
+    // Wait for the video to finish downloading
+    videoWritableStream.on('finish', () => {
+        console.log("Video downloaded successfully");
+        res.status(200).send("Video downloaded successfully");
+    });
+
+    // Handle any errors that occur during the download process
+    videoWritableStream.on('error', (error) => {
+        console.error("Error downloading the video: ", error);
+        res.status(500).send("Error downloading the video");
+    });
+});
+
+app.use('/', router);
 app.listen(8080, () => {
-    console.log('app listingin on 8080')
-})
+    console.log('app listening on port 8080')
+});
